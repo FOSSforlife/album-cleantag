6a0ee03e4538792a6e4c19a01b2c84cc1c327e11
Elias Jackson <ejthedj0@gmail.com>
2019-01-08 17:42:28 -0800

    Smart regex works

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const fs = require('fs');
 
-let unwantedSuffixes = JSON.parse(fs.readFileSync('./suffixes.json'));
-unwantedSuffixes = '(' + unwantedSuffixes.join('|') + ')';
+let redFlags = 'bonus|deluxe|edition|expanded|explicit|remaster|version';
 
-let regex = new RegExp('(\\(|\\[|- )' + unwantedSuffixes + '(\\)|\\])?(\\)|\\])?', 'gi');
+let regex = new RegExp('(\\(|\\[)(.*)(' + redFlags + ')(.*)(\\)|\\])(\\)|\\])?', 'gi');
+let regex2 = new RegExp('(- )(.*)(' + redFlags + ')(.*)', 'gi');
 
 module.exports.clean = function (name) {
-    return name.replace(regex, '').trim();
+    // filters parentheses and brackets
+    name = name.replace(regex, '');
+
+    // searches for hyphens (e.g. 'Title - Remastered')
+    if(name.match(/-/g) === null) {
+        return name;
+    }
+    else {
+        if(name.match(/-/g).length === 1) {
+            return name.replace(regex2, '').trim();
+        }
+        else {
+            const lastHyphen = name.lastIndexOf('-');
+            const firstBit = name.substring(0, lastHyphen);
+            return firstBit + name.substr(lastHyphen).replace(regex2, '').trim();
+        }
+    }
+    
 };
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const cleantag = require('./index');
-let albumName = 'Godflesh - Post Self (Remastered)';
+let albumName = 'Godflesh - Post Self - sdf - sdf - sdf - - Remastered';
 albumName = cleantag.clean(albumName);
 
 console.log(albumName); // Godflesh - Post Self
